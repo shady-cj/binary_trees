50977d70ab574e65c51eb58d55b0a69f0a6be6a0
shady-cj <petersp2000@gmail.com>
2022-11-15 22:28:20 +0100

    Second task commit message

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,9 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_insert_left *new_node;
+	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_insert_left));
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
