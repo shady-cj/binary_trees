ff4da1b90d4c2d28ae8656026876c9a7efe9e405
shady-cj <petersp2000@gmail.com>
2022-11-15 22:37:37 +0100

    Second task commit message

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,5 +21,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 		new_node->left = parent->left;
 	parent->left = new_node;
-	return (parent);
+	return (new_node);
 }
