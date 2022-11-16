298fb6edb55967da003d0f1e3ca493e1bd4ac2f4
shady-cj <petersp2000@gmail.com>
2022-11-16 20:46:40 +0100

    Insert right task implementation

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -18,6 +18,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->left = NULL;
+	new_node->right = NULL;
 	new_node->n = value;
 	new_node->parent = parent;
 	if (parent->right)
