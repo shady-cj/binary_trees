744e249f1d9cab776487b2265f3a0bee4c032a66
rencyngina <rencyngina@gmail.com>
2022-11-16 02:34:15 -0800

    changrd code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,6 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -23,7 +24,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 	if (parent->left != NULL)
+	{
 		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 	parent->left = new_node;
+
 	return (new_node);
 }
